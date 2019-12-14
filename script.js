b1f2980ea5fa9aea971c84f9f0031ec893d27f45
Aleksandr Miedviediev <MyStockWork2017@gmail.com>
2019-12-14 19:00:07 +0200

    правильный скрипт , но почему то не работает

--- script.js
+++ script.js
@@ -5,69 +5,18 @@
 // enter, space, alt, shift, ctrl. Затемнение реализуйте через добавление класса CSS. Для удобства рекомендую каждой
 // клавише добавить атрибут data с символом. Если нажата клавиша caps lock - то присвоить ей затемнение, которое работает
 //  до последующего отжатия клавиши
-let number1 = document.querySelectorAll(".buttoN").attributes;
-// let atr = number1.getAttribute('data');
+
+
+// Есть множество div элементов с одинаковым классом.При переборе я должен найти тот div в блоке которого указано например слово 'hello'
+// и присвоить ему новый класс.
+
 let inp = document.querySelector("#inp");
-// let dat = document.getAttribute('.button data');
-inp.onkeypress = function () {
-    console.log("ura");
-    for (i = 0; i < number1.length; i++) {
-        if (inp.value == atr) {
-            this.number1.classList.toggle(".buthover");
-        }
-    }
+let but = document.querySelectorAll('.buttoN');
+inp.onkeydown = function (e) {
+	for (var i = 0; i < but.length; i++) {
+		if (but[i].textContent == e.key) {
+			but[i].classList.toggle("yellow");
+			console.log('but[i]');
+		}
+	}
 }
-
-
-
-
-
-
-
-
-// inp.onkeypress = function (event) {
-//     // if (dat == 1) {
-//     //     number1.classList.toggle(".buthover");
-//     // }
-
-
-
-//     const a = {
-//         charcode1: '1',
-//         charcode2: '2',
-//         charcode3: '3'
-//     }
-//     for (elem in a) {
-//         if (inp.value == a[event.key]) {
-//             number1.classList.toggle(".buthover");
-//         }
-//     }
-//     console.log(event);
-// }
-
-
-
-
-
-
-// let number2 = document.querySelector(".div-2");
-// keyBoard.onkeydown = function () {
-//     number2.classList.toggle("buthover");
-// }
-
-// let number3 = document.querySelector(".div-3");
-// keyBoard.onkeydown = function () {
-//     number3.classList.toggle("buthover");
-// }
-
-// let buttoN = document.querySelector(".buttoN");
-// keyBoard.onkeydown = function (event) {
-//     const number = {
-//         1: 2,
-//         2: 3,
-//         3: 4
-//     }
-//     document.querySelector("#keyBoard").value += number[event.key];
-//     console.log(event.key);
-
-// }
